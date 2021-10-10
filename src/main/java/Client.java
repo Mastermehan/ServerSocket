979fee9a3f58eb8897302439a70123d41d3c254d
Mastermehan <mastermehan1984@gmail.com>
2021-10-11 02:46:18 +0300

    ServerSocket

---
+++ src/main/java/Client.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) throws Exception {
+        String host = "localhost";
+        int port = 8080;
+
+        try (Socket clientSocket = new Socket(host,port);
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))) {
+
+                 out.println("Netology\n");
+
+                 String resp = in.readLine();
+                 System.out.println(resp);
+        }
+
+        //определение IP
+        InetAddress inetAddress = InetAddress.getByName(host);
+        System.out.println(host + ", ip address: " +
+                inetAddress.getHostAddress());
+    }
+}
